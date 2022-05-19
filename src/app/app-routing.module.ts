69db06b3177872233a2e446c668c273c9115a045
Luca Caboni <99878411+luca-caboni@users.noreply.github.com>
2022-05-19 14:22:10 +0200

    added main components and services, added routing and guard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports:[RouterModule]
-})
-export class AppRoutingModule { }
+const routes: Route[] = [
+  {
+    path: 'movies',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./features/movies/movies.module').then(
+        (m) => m.MoviesModule
+      )
+  },
+
+  {
+    path: 'favourites',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./features/favourites/favourites.module').then((m) => m.FavouritesModule)
+  }]
+
+  @NgModule({
+    imports: [
+      RouterModule.forRoot(routes)
+    ],
+    exports:[RouterModule]
+  })
+  export class AppRoutingModule { }
+
+
